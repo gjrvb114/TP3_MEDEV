2797a66b5ec064cb5cf7d6f60ec20bd490940e31
gjrvb114 <gjrvb114@gmail.com>
2016-12-08 12:07:40 +0100

    ça marche enfin...

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 CC = g++
 
-#GTESTLIB = "/home/user/Bureau/googletest"
+GTESTLIB = /home/user/Bureau/googletest
 GTESTINC = "/home/user/Bureau/googletest/include"
 
 all: JeuDeGo
 
 JeuDeGo: main.o
-	$(CC) -o $@ $^ libgtest.a
+	$(CC) -o $@ $^ $(GTESTLIB)/libgtest.a -pthread
 
 main.o: main.cpp
 	$(CC) -o $@ -c $< -std=c++11 -I$(GTESTINC)
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "gtest/gtest.h"
-#include <iostream>
+
+TEST (Testtest, egalite) { 
+    ASSERT_EQ (0.0, 1.0);
+    ASSERT_EQ (-1, -1);
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World" << std::endl;
-	std::string c;
-	std::cin >> c;
-	return 0;
+	::testing::InitGoogleTest(&argc, argv);
+  	return RUN_ALL_TESTS();
 }
