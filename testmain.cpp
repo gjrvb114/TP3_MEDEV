ec1f09dc5a800f32da566ef567ea6f919c5c0b71
gjrvb114 <gjrvb114@gmail.com>
2016-12-15 11:12:42 +0100

    Changing things

--- testmain.cpp
+++ testmain.cpp
@@ -1,36 +1,12 @@
-/*
- * testmain.cpp
- *
- *  Created on: 8 déc. 2016
- *      Author: user
- */
-
-
-
-// gtest_main.cpp
-#include <stdio.h>
 #include "gtest/gtest.h"
-#include <iostream>
-#include "Goban.h"
 using namespace std;
 
-GTEST_API_ int main(int argc, char **argv) {
-  printf("Running main() from gtest_main.cc\n");
+TEST(GobanClass,PoserPierre){
+    EXPECT_EQ(true,1);
+    EXPECT_EQ(false,0);
+}
+
+int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
-
-/*
-int main()
-{
-	Goban machin = Goban();
-	machin.Affichage();
-	cout << endl ;
-	machin.PoserPierre(2,2);
-	machin.SetJoueur();
-	machin.PoserPierre(3,3);
-	machin.Affichage();
-	return 0;
-}
-*/
