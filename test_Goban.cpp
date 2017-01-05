224667a530581b85a4bf2685992d3739b97f0cad
paulouis <paulouis.gomichel@gmail.com>
2017-01-05 08:42:29 +0100

    l

--- test_Goban.cpp
+++ test_Goban.cpp
@@ -47,21 +47,4 @@ TEST (GobanClass, LiberteGroupe)
 	EXPECT_EQ(2,machin.LiberteGroupe(1,3));
 
 }
-/*
-TEST(IntegerFunctionTest, negative) {
-        EXPECT_EQ(1, factorial(-5));
-        EXPECT_EQ(1, factorial(-1));
-        EXPECT_GT(factorial(-10), 0);
-}
 
-TEST(IntegerFunctionTest, DISABLED_zero) {
-         EXPECT_EQ(1, factorial(0));
-}
-
-TEST(IntegerFunctionTest, postive) {
-         EXPECT_EQ(1, factorial(1));
-         EXPECT_EQ(2, factorial(2));
-         EXPECT_EQ(6, factorial(3));
-         EXPECT_EQ(40320, factorial(8));
-}
-*/
